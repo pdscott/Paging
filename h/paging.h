e76df6ed7e5147b5e14c9acd4c2c70b2ae3c1a29
pdscott2 <github@preston.fastmail.fm>
2018-04-23 13:26:10 -0400

    various bug fixes

--- h/paging.h
+++ h/paging.h
@@ -71,7 +71,7 @@ int get_bs(bsd_t, unsigned int);
 SYSCALL release_bs(bsd_t);
 SYSCALL read_bs(char *, bsd_t, int);
 SYSCALL write_bs(char *, bsd_t, int);
-SYSCALL get_frm(int* avail) 
+SYSCALL get_frm(int* avail);
 
 #define NBPG		4096	/* number of bytes per page	*/
 #define FRAME0		1024	/* zero-th frame		*/
