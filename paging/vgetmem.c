d372e0624e6b513b10bbaebdaffb927f03371081
pdscott2 <github@preston.fastmail.fm>
2018-04-23 17:29:09 -0400

    bug fixes

--- paging/vgetmem.c
+++ paging/vgetmem.c
@@ -12,11 +12,44 @@ extern struct pentry proctab[];
  *------------------------------------------------------------------------
  */
 WORD	*vgetmem(nbytes)
-	unsigned nbytes;
-{
+	unsigned nbytes; {
 
-	kprintf("To be implemented!\n");
-	return( SYSERR );
+	STATWORD ps;
+	struct	mblock	*p, *q, *leftover;
+	struct pentry *pptr;
+
+	disable(ps);
+	// Sanity check on memory request
+	if (nbytes==0 || nbytes > 128*NBPG) {
+		restore(ps);
+		return( (WORD *)SYSERR);
+	}
+
+	// Ensure there is memory left
+	pptr = &proctab[currpid];
+	if (pptr->vmemlist.mnext == NULL) {
+		restore(ps);
+		return((WORD *)SYSERR);
+	}
+	// Follow same approach as create.c
+	nbytes = (unsigned int) roundmb(nbytes);
+	for (q= pptr->vmemlist,p=pptr->memlist->mnext ;
+	     p != (struct mblock *) NULL ;
+	     q=p,p=p->mnext)                               {
+
+		if ( p->mlen == nbytes) {
+			q->mnext = p->mnext;
+			restore(ps);
+			return( (WORD *)p );
+		} else if (p->mlen > nbytes) {
+			leftover = (struct mblock *)( (unsigned)p + nbytes );
+			q->mnext = leftover;
+			leftover->mnext = p->mnext;
+			leftover->mlen = p->mlen - nbytes;
+			restore(ps);
+			return((WORD *)p);
+		}
+		restore(ps);
+		return((WORD *)SYSERR);
+	}
 }
-
-
