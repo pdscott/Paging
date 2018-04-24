dc6894d82735d35ff6be8497a27c3c385b8beaff
pdscott2 <github@preston.fastmail.fm>
2018-04-23 21:12:38 -0400

    bug fixes

--- paging/pfint.c
+++ paging/pfint.c
@@ -2,6 +2,8 @@
 
 #include <conf.h>
 #include <kernel.h>
+#include <mem.h>
+#include <proc.h>
 #include <paging.h>
 
 
@@ -25,7 +27,7 @@ SYSCALL pfint() {
     
 	// 3. check if addr is legal
 	status = bsm_lookup(currpid, addr, &store, &pageth);
-	if (status == SYSERR) { return(SYSERR)};
+	if (status == SYSERR) { return(SYSERR); }
 
 	// 5,6,7 If page table doesn't exist, make it.
 	pptr = &proctab[currpid];
@@ -35,31 +37,30 @@ SYSCALL pfint() {
 		if (ptentry == SYSERR) { kill(currpid); }
 		pdentry[pdoffset].pd_pres = 1;
 		pdentry[pdoffset].pd_write = 1;
-		pdentry[pdoffset].pd_base = ptentry >> 12;
+		pdentry[pdoffset].pd_base = (unsigned long) ptentry >> 12;
 		// frame num = this address - base address / 4096
-		frame = (pdentry - (FRAME0* NBPG)) / NBPG
+		frame = ((unsigned long) pdentry - (FRAME0* NBPG)) / NBPG;
 		frm_tab[frame].fr_vpno = addr >> 12;
 		if (grpolicy() == SC) {
-			frm_tab[frame].scbit == 1;
+			frm_tab[frame].fr_scbit == 1;
 		}
 	} else {
 		// just note location of page table
-		ptentry = pdentry[pdoffset].pdbase << 12;
+		ptentry = pdentry[pdoffset].pd_base << 12;
 	}
 
 	// Page in faulted pages
-	if (ptentry[ptoffset].pt_pres != 1) {
+	if (ptentry[ptoffset].pt_pres == 0) {
 		status = get_frm(frame);
 		if (ptentry == SYSERR) { kill(currpid); }
 		frm_tab[frame].fr_vpno = addr >> 12;
-		frm_tab[frame].refcnt++;
+		frm_tab[frame].fr_refcnt++;
 		if (grpolicy() == SC) {
-			frm_tab[frame].scbit == 1;
+			frm_tab[frame].fr_scbit == 1;
 		}
 		faddr = FRAME0*NBPG + frame*NBPG;
 		status = read_bs(faddr, store, pageth);
 		if (status == SYSERR) { kill(currpid); } 
-		frm_tab[frame].fr_bsid = store;
 		ptentry[ptoffset].pt_pres = 1;
 		ptentry[ptoffset].pt_write = 1;
 		ptentry[ptoffset].pt_base = faddr >> 12;
